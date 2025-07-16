69bdff64516725f8eebb09ed7e14278c0e0d0e1d
Abuuuba <yannissebih@gmail.com>
2025-07-16 16:47:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ const App = () => {
     salade: [
       { name: 'Salade Tenders', price: '10€ (Seul) / 13€ (Menu)', ingredients: 'Tenders, Tomates, Olives, Fêta fraîche, Frites + Oignons frits' },
       { name: 'Salade Tikka', price: '10€ (Seul) / 13€ (Menu)', ingredients: 'Poulet Tikka, Tomates, Olives, Fêta fraîche, Frites + Oignons frits' },
-      { name: 'Salade Royal', price: '12€ (Seul) / 15€ (Menu)', ingredients: '2 viandes au choix, Tomates, Olives, Fêta fraîche, Frites + Oignons frits' }
+      { name: 'Salade Royal', price: '12€ (Seul) / 15€ (Menu)', ingredients: '2 viandes au choix, Tomates, Olives, Fêta fraîche, Frites + Oignons frits' },
       { name: 'Chicken Tandori', price: '10€ (Seul) / 13€ (Menu)', ingredients: 'Salade, Tomates, Olives, Fêta fraîche ou Frites + Oignons frits' },
       { name: 'KING Chicken Tandori', price: '12€ (Seul) / 15€ (Menu)', ingredients: 'Cuisse de poulet Tandoori, Tomates, Olives, Fêta fraîche ou Frites + Oignons frits' },
       { name: 'Assiette Simple', price: '10€ (Seul) / 12€ (Menu)', ingredients: '1 viande au choix, Tomates, Olives, Fêta fraîche, Frites + Oignons frits' },
